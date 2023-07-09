c14cb5fbcf2211c89b7f2b662c6edecce61e4f59
Evangeline Wong <wong.evangeline77@gmail.com>
2023-07-09 15:37:08 -0400

    async pipe and initialzing firebase first

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,7 +8,12 @@ import { ModalService } from '../services/modal.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  constructor(public modal: ModalService) { }
+
+  constructor(
+    public modal: ModalService,
+    public auth: AuthService
+    ) {
+     }
 
   ngOnInit(): void {
 
