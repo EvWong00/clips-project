6053e218b5cae382417a55b2bb9b8c0ca7781c87
Evangeline Wong <wong.evangeline77@gmail.com>
2023-07-10 18:01:59 -0400

    designed upload page

--- src/app/video/video.module.ts
+++ src/app/video/video.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { VideoRoutingModule } from './video-routing.module';
 import { ManageComponent } from './manage/manage.component'
 import { FormsModule } from '@angular/forms';
+import { UploadComponent } from './upload/upload.component';
 
 
 @NgModule({
   declarations: [
     ManageComponent,
+    UploadComponent,
     
   ],
   imports: [
