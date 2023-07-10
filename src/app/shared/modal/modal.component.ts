875ceec4aea024c7c4c84e4088a59f60fa9f3240
Evangeline Wong <wong.evangeline77@gmail.com>
2023-07-09 22:41:06 -0400

    destorying the modal

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ElementRef } from '@angular/core';
+import { Component, Input, OnInit, ElementRef, OnDestroy } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -7,7 +7,7 @@ import { ModalService } from 'src/app/services/modal.service';
   styleUrls: ['./modal.component.css'],
  // providers: [ModalService]
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy{
   @Input() modalID = ''
 
   constructor(
@@ -19,6 +19,10 @@ export class ModalComponent implements OnInit {
 
   ngOnInit(): void {
     document.body.appendChild(this.el.nativeElement)
+  }
+
+  ngOnDestroy() {
+    document.body.removeChild(this.el.nativeElement)
   }
 
   closeModal() {
