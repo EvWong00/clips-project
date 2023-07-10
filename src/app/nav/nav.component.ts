58b2257ca56abbbb0d1e183ac496e5524a1d43ed
Evangeline Wong <wong.evangeline77@gmail.com>
2023-07-10 10:27:22 -0400

    logout functionality added

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,8 @@ export class NavComponent implements OnInit {
 
   constructor(
     public modal: ModalService,
-    public auth: AuthService
+    public auth: AuthService,
+    private afAuth: AngularFireAuth
     ) {
      }
 
@@ -24,4 +26,11 @@ export class NavComponent implements OnInit {
 
     this.modal.toggleModal('auth')
   }
+
+  async logout($event: Event) {
+    $event.preventDefault()
+
+    await this.afAuth.signOut()
+  }
+
 }
