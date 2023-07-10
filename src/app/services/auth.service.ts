163c1d8a4cc231157c605c9734d6e61ad2ad8f69
Evangeline Wong <wong.evangeline77@gmail.com>
2023-07-09 22:48:28 -0400

    delay operator for destorying modal

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,6 +4,7 @@ import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/comp
 import IUser from '../models/user.model';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ import { map } from 'rxjs';
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>
   public isAuthenticated$: Observable<boolean>
+  public isAuthenticatedWithDelay$: Observable<boolean>
 
   constructor(
     private auth: AngularFireAuth,
@@ -19,6 +21,9 @@ export class AuthService {
     this.usersCollection = db.collection('users')
     this.isAuthenticated$ = auth.user.pipe(
       map(user =>!!user)
+    )
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(
+      delay(1000)
     )
    }
 
