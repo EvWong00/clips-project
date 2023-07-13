1123f6d1c12dadb82d685b43bbeee1e01fc251f3
Evangeline Wong <wong.evangeline77@gmail.com>
2023-07-13 12:46:14 -0400

    fallback upload method

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -50,9 +50,12 @@ export class UploadComponent implements OnInit {
   }
 
   storeFile($event: Event ) { 
+    
     this.isDragover = false
 
-    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+    this.file = ($event as DragEvent).dataTransfer ? 
+    ($event as DragEvent).dataTransfer?.files.item(0) ?? null :
+    ($event.target as HTMLInputElement).files?.item(0) ?? null
 
     if(!this.file || this.file.type !== 'video/mp4') {
       return
@@ -105,7 +108,7 @@ export class UploadComponent implements OnInit {
       },
       error: (error) => {
         this.uploadForm.enable()
-        
+
         this.alertColor = 'red'
         this.alertMessage = 'Upload failed. Please try again later.'
         this.inSubmission = true
