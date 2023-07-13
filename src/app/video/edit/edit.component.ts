8aaa36fe400f8a8eb915d39337ea8198bc402627
Evangeline Wong <wong.evangeline77@gmail.com>
2023-07-13 17:00:41 -0400

    able to edit video names

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, OnDestroy, Input} from '@angular/core';
+import { 
+  Component, OnInit, OnDestroy, Input, OnChanges, SimpleChanges
+} from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 import IClip from 'src/app/models/clip.model';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
@@ -8,9 +10,24 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent implements OnInit, OnDestroy {
+export class EditComponent implements OnInit, OnDestroy, OnChanges {
   @Input() activeClip: IClip | null = null 
-  
+
+  clipID = new FormControl('', {
+    nonNullable: true
+  })
+  title = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(3)
+    ],
+    nonNullable: true
+  })
+  editForm = new FormGroup ({
+    title: this.title,
+    id: this.clipID
+  })
+
   constructor(private modal: ModalService) {
 
   }
@@ -22,4 +39,16 @@ export class EditComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
       this.modal.unregister('editClip')
   }
+
+  ngOnChanges() {
+    if (!this.activeClip) {
+      return;
+    }
+  
+    this.clipID.setValue(this.activeClip.docID || '');
+    this.title.setValue(this.activeClip.title);
+  }
+  
+
+  
 }
