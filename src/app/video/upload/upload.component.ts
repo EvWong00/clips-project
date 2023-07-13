8670a90a431753659206519a30e38ebb300d0031
Evangeline Wong <wong.evangeline77@gmail.com>
2023-07-13 12:55:23 -0400

    canceling upload

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/compat/storage';
 import { v4 as uuid } from 'uuid';
 import { last, switchMap } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
@@ -13,7 +13,7 @@ import IClip from 'src/app/models/clip.model';
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css']
 })
-export class UploadComponent implements OnInit {
+export class UploadComponent implements OnDestroy {
   isDragover = false
   file: File | null = null
   nextStep = false
@@ -24,6 +24,7 @@ export class UploadComponent implements OnInit {
   percentage = 0
   showPercentage = false
   user: firebase.User | null = null
+  task?: AngularFireUploadTask
 
   title = new FormControl('', {
     validators: [
@@ -45,8 +46,8 @@ export class UploadComponent implements OnInit {
       auth.user.subscribe(user => this.user = user)
     }
 
-  ngOnInit(): void {
-      
+  ngOnDestroy(): void {
+      this.task?.cancel()
   }
 
   storeFile($event: Event ) { 
@@ -79,13 +80,13 @@ export class UploadComponent implements OnInit {
     const clipFileName = uuid()
     const clipPath = `clips/${clipFileName}.mp4`
 
-    const task = this.storage.upload(clipPath, this.file)
+    this.task = this.storage.upload(clipPath, this.file)
     const clipRef = this.storage.ref(clipPath)
 
-    task.percentageChanges().subscribe(progress => {
+    this.task.percentageChanges().subscribe(progress => {
       this.percentage = progress as number / 100
     })
-    task.snapshotChanges().pipe(
+    this.task.snapshotChanges().pipe(
       last(),
       switchMap(() => clipRef.getDownloadURL())
     ).subscribe({
