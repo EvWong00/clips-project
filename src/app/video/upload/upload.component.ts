9a742f145a887f498e014096273a745f8da7fbed
Evangeline Wong <wong.evangeline77@gmail.com>
2023-07-20 13:55:57 -0400

    added border to only selected screenshot

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -27,6 +27,7 @@ export class UploadComponent implements OnDestroy {
   user: firebase.User | null = null
   task?: AngularFireUploadTask
   screenshots: string[] = []
+  selectedScreenshot = ''
 
   title = new FormControl('', {
     validators: [
@@ -71,6 +72,8 @@ export class UploadComponent implements OnDestroy {
     }
 
     this.screenshots = await this.ffmpegService.getScreenshots(this.file)
+
+    this.selectedScreenshot = this.screenshots[0]
 
     this.title.setValue(
       this.file.name.replace(/\.[^/.]+$/, '')
