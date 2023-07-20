988839d3aacd9c293bd0cc11385adf4e363a8e51
Evangeline Wong <wong.evangeline77@gmail.com>
2023-07-20 19:19:17 -0400

    uploading a blob

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -28,6 +28,7 @@ export class UploadComponent implements OnDestroy {
   task?: AngularFireUploadTask
   screenshots: string[] = []
   selectedScreenshot = ''
+  screenshotTask?: AngularFireUploadTask
 
   title = new FormControl('', {
     validators: [
@@ -81,7 +82,7 @@ export class UploadComponent implements OnDestroy {
     this.nextStep = true
   }
 
-  uploadFile() {
+  async uploadFile() {
     this.uploadForm.disable()
 
     this.showAlert = true
@@ -93,8 +94,17 @@ export class UploadComponent implements OnDestroy {
     const clipFileName = uuid()
     const clipPath = `clips/${clipFileName}.mp4`
 
+    const screenshotBlob = await this.ffmpegService.blobFromURL(
+      this.selectedScreenshot
+    )
+    const screenshotPath = `screenshots/${clipFileName}.png`
+
     this.task = this.storage.upload(clipPath, this.file)
     const clipRef = this.storage.ref(clipPath)
+
+    this.screenshotTask = this.storage.upload(
+      screenshotPath, screenshotBlob
+      )
 
     this.task.percentageChanges().subscribe(progress => {
       this.percentage = progress as number / 100
