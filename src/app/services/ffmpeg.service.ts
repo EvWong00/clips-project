b38af916ec99499b283e9a26ff8a2df91e52724a
Evangeline Wong <wong.evangeline77@gmail.com>
2023-07-20 00:34:42 -0400

    generating a screenshot

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -26,6 +26,19 @@ export class FfmpegService {
     const data = await fetchFile(file)
 
     this.ffmpeg.FS('writeFile', file.name, data)
+
+    await this.ffmpeg.run(
+      //Input
+      '-i', file.name,
+
+      //Output Options
+      '-ss', '00:00:01',
+      '-frames:v', '1',
+      '-filter:v', 'scale=510:-1',
+
+      //Output
+      'output_01.png'
+    )
    }
 
 }
