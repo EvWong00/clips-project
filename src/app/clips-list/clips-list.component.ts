3b5754305bcf9875374a7098d755fa4e5e3a252f
Evangeline Wong <wong.evangeline77@gmail.com>
2023-07-21 15:23:56 -0400

    rendering clips on homepage

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ClipService } from '../services/clip.service';
 
 @Component({
   selector: 'app-clips-list',
@@ -6,7 +7,10 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./clips-list.component.css']
 })
 export class ClipsListComponent implements OnInit, OnDestroy {
-  constructor() { }
+
+  constructor(public clipService: ClipService) {
+      this.clipService.getClips()
+   }
 
   ngOnInit(): void {
       window.addEventListener('scroll', this.handleScroll)
@@ -23,7 +27,7 @@ export class ClipsListComponent implements OnInit, OnDestroy {
     const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight
 
     if(bottomOfWindow) {
-      console.log('bottom of window')
+      this.clipService.getClips()
     }
   }
 
