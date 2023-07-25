7cdfbbde9f7f56b3b84fa7dee76706fb8467ea12
Evangeline Wong <wong.evangeline77@gmail.com>
2023-07-25 16:52:32 -0400

    dynamically rendering vid and copy/paste links

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -2,23 +2,33 @@ import { Component, OnInit, ViewChild, ElementRef, ViewEncapsulation } from '@an
 import { ActivatedRoute, Params } from '@angular/router';
 import videojs from 'video.js';
 import Player from "video.js/dist/types/player";
+import IClip from '../models/clip.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
   styleUrls: ['./clip.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  providers: [DatePipe]
 })
 export class ClipComponent implements OnInit {
-  id = ''
   @ViewChild('videoPlayer', { static: true }) target?: ElementRef
   player?: Player
+  clip?: IClip
 
   constructor(public route: ActivatedRoute) { }
 
   ngOnInit(): void {
-      this.route.params.subscribe((params: Params) => {
-        this.id = params['id']
+      this.player = videojs(this.target?.nativeElement)
+
+      this.route.data.subscribe(data => {
+        this.clip = data['clip'] as IClip
+
+        this.player?.src({
+          src: this.clip.url,
+          type: 'video/mp4'
+        })
       })
   }
 
