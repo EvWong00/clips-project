61d4739b93e2b8e3089ac2f634524180205297a8
Evangeline Wong <wong.evangeline77@gmail.com>
2023-07-26 00:34:39 -0400

    skipLibCheck: true

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,21 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import { AboutComponent } from "./about.component";
 
-import { AboutComponent } from './about.component';
-
-describe('AboutComponent', () => {
-  let component: AboutComponent;
-  let fixture: ComponentFixture<AboutComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [AboutComponent]
-    });
-    fixture = TestBed.createComponent(AboutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+describe('About Component', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AboutComponent],
+    }).compileComponents();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+})
