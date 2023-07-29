77a69f56247cef05a50c42be0d1bb813fbee921e
Evangeline Wong <wong.evangeline77@gmail.com>
2023-07-28 20:59:16 -0400

    change to cypress test

--- cypress/e2e/clip.cy.ts
+++ cypress/e2e/clip.cy.ts
@@ -1,10 +1,12 @@
 describe('Clip', () => {
     it('should play clip', () => {
-        cy.visit('/');
-        cy.get('app-clips-list > .grid a:first').click();
-        cy.get('.video-js').click();
-        cy.wait(3000);
-        cy.get('.video-js').click();
-        cy.get('.vjs-play-progress').invoke('width').should('gte', 0);
+      cy.visit('/');
+      cy.wait(3000);
+      cy.get('app-clips-list > .grid a:first').click();
+      cy.wait(3000);
+      cy.get('.vjs-big-play-button').click();
+      cy.wait(3000);
+      cy.get('#vjs_video_3_html5_api').click();
+      cy.get('.vjs-play-progress').invoke('width').should('gte', 0);
     });
-});
+  });
